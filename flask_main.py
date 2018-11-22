7a20fcc4e669eada8055f467500e50ca2d2a1c64
CaptainMumo <mumopeter17@gmail.com>
2018-11-22 17:12:02 +0300

    Creted sign in route

--- flask_main.py
+++ flask_main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, redirect, flash
-from forms import SignupForm
+from forms import SignupForm, SigninForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e8f836430f6c0c054ffe0231a7158fe1'
@@ -14,6 +14,13 @@ def signup():
     if form.validate_on_submit():
         return redirect(url_for('home'))
     return render_template('signup.html', title='Sign Up', form=form)
+
+@app.route("/signin", methods=['GET', 'POST'])
+def signin():
+    form = SigninForm()
+    if form.validate_on_submit():
+        return redirect(url_for('home'))
+    return render_template('signin.html', title='Sign In', form=form)
 
 if __name__=="__main__":
     app.run(debug=True)
