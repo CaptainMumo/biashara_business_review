fbc908c6a701f270951dc12adfdbec5edb1069cf
CaptainMumo <mumopeter17@gmail.com>
2018-11-22 15:25:38 +0300

    Created signup route

--- flask_main.py
+++ flask_main.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, flash
+from forms import SignupForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'e8f836430f6c0c054ffe0231a7158fe1'
 
 @app.route("/")
 def home():
     return render_template('home.html')
 
-
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        return redirect(url_for('home'))
+    return render_template('signup.html', title='Sign Up', form=form)
 
 if __name__=="__main__":
     app.run(debug=True)
