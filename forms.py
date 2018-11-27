e9deca13bde404e03f170151cb84a10f2b436022
CaptainMumo <mumopeter17@gmail.com>
2018-11-27 19:41:40 +0300

    Added function to check if username has already been taken during sign up

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+#from flask_main import users
 
 class SignupForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=2, max=15)])
@@ -9,6 +10,10 @@ class SignupForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired(), Length(min=8,message="Password must be at least 8 characters long")])
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Sign Up")
+
+    """def validate_username(self, field):
+        if self.username == (user['username'] for user in users):
+            raise ValidationError("Username has already been taken")"""
 
 class SigninForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
