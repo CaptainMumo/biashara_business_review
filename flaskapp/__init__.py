386500f62a52ae62bc27104ed196d387298f6d2e
CaptainMumo <mumopeter17@gmail.com>
2018-11-30 10:31:43 +0300

    add sqlalchemy and sqlite to create database

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e8f836430f6c0c054ffe0231a7158fe1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
