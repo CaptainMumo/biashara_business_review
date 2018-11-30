6cabc32affbc6d1fc32feebf2c77722704e8cfab
CaptainMumo <mumopeter17@gmail.com>
2018-11-30 10:32:24 +0300

    add classes to describe user, business and review models for the database

---
+++ flaskapp/models.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from flaskapp import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    location = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    business = db.relationship("Business", backref="owner", lazy=True)
+    review = db.relationship("Review", backref="author", lazy=True)
+
+    def __repr__(self):
+        return f"Username : {self.username} Email : {self.email} Location : {self.location}"
+
+
+class Business(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    location = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    phone = db.Column(db.String(15), unique=True, nullable=False)
+    business_owner = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    review = db.relationship("Review", backref="business", lazy=True)
+
+    def __repr__(self):
+        return f"Business name : {self.name} Category : {self.category} Location : {self.location} Email : {self.email} Phone no. : {self.phone}"
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    posted_by = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    posted_for = db.Column(db.Integer, db.ForeignKey("business.id"), nullable=False)
+
+    def __repr__(self):
+        return f"Title : {self.title} Posted by : {self.posted_by} For : {self.posted_for} On : {self.date_posted}"
