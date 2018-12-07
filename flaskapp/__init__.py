deddfed7201cf93372fd757a2591281bf84f008c
CaptainMumo <mumopeter17@gmail.com>
2018-12-07 12:47:02 +0300

    update configurations

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -3,9 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e8f836430f6c0c054ffe0231a7158fe1'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
